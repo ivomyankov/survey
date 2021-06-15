2bc2a6ce3354ba799d1774096edb17dbe6e55051
ivomyankov <ivomyankov@gmail.com>
2021-06-15 14:34:25 +0300

    bug fix for empty results

--- app/Http/Livewire/Results/FinalQuestion.php
+++ app/Http/Livewire/Results/FinalQuestion.php
@@ -23,7 +23,7 @@ class FinalQuestion extends Component
     private function options()
     {
         foreach ($this->element as $key => $elem) {
-            if ( $key > 0 ) {
+            if ( $key > 0 && array_key_exists('q'.$elem->id, $this->results ) ) {
                 $this->options[$key] = array_sum($this->results['q'.$elem->id])/count($this->results['q'.$elem->id]);
             }
         }
