03723f0b2602cd735ea441bda004f78c37382800
ivomyankov <ivomyankov@gmail.com>
2021-07-29 17:21:22 +0300

    added CORD middleware
    added CORD middleware, registered in karnel and called in api route

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/h/{hash}', [FormController::class, 'ajaxPostForm'])->name('ajaxPostForm');
+Route::middleware(['cors'])->post('/h/{hash}', [FormController::class, 'ajaxPostForm'])->name('ajaxPostForm');
 
 
 
