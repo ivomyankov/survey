acd2beb374b377f1a7c6476fd170b00db389e999
ivomyankov <ivomyankov@gmail.com>
2021-09-20 13:17:27 +0300

    text element fix
    added ID on text elements

--- resources/views/livewire/front/text-element.blade.php
+++ resources/views/livewire/front/text-element.blade.php
@@ -1,4 +1,4 @@
-<div class="rounded shadow bg-light p-4 position-relative {{ $element[0]->visible === 0 ? 'hidden' : '' }} mt-5" alt="{{$element[0]->id}}">
+<div id="{{$element[0]->id}}" class="rounded shadow bg-light p-4 position-relative {{ $element[0]->visible === 0 ? 'hidden' : '' }} mt-5" alt="{{$element[0]->id}}">
     <b>{{$element[0]->text}} </b> 
     <br><br>
     <textarea class="w-100 input-border q{{$element[0]->id}}" type="text" rows="{{$rows}}" name="q{{$element[0]->id}}_t"></textarea>
